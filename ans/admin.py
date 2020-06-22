6adbeca72a48a3fb22f698b552d970b22e075ae0
Mohit_Gupta <52665879+Mohit-15@users.noreply.github.com>
2020-06-22 13:06:31 +0530

    Add filters
     Filters for the title to get all the objects of a particular title in the model.

--- ans/admin.py
+++ ans/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Question , Answer
 
 # Register your models here.
-admin.site.register(Question)          # registering the models 
-admin.site.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+	model = Answer
+	list_filter = ('ans_title',)
+
+class QuestionAdmin(admin.ModelAdmin):
+	model = Question
+	list_filter = ('ques_title',)
+
+admin.site.register(Question, QuestionAdmin)          # registering the models 
+admin.site.register(Answer, AnswerAdmin)
 
